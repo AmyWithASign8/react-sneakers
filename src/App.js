1566b7217ea14870ab09dff6c216fe2e7d185633
DanilFrontend <danilstorage@gmail.com>
2022-07-02 01:55:21 +0500

    3st step layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-import {Header, Card} from './components';
+import {Header, Card, Drawer} from './components';
+
+const arrCard = [
+  {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.jpg'},
+  {title: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl: '/img/sneakers/2.jpg'},
+  {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/sneakers/3.jpg'},
+  {title: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: '/img/sneakers/4.jpg'},
+]
 
 function App() {
   return (
     <div className="wrapper clear">
+    <Drawer/>
     <Header/>
     <div className="content p-40">
       <div className='d-flex align-center justify-between mb-40'>
@@ -14,11 +22,11 @@ function App() {
         </div>
       </div>
       <div className="d-flex">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-
+        {
+          arrCard.map((obj) => (
+              <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} onClick={() => console.log(obj)}/>
+          ))
+        }
       </div>
     </div>
   </div>
---
+++ src/components/Card/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './Card.module.scss';
+
+function Card({title, price, imageUrl, onClick}) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.favorite}>
+        <img src="/img/heart-unliked.svg" alt="Unliked" />
+      </div>
+      <img width={133} height={112} src={imageUrl} alt="Sneakers" />
+      <h5>{title}</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена:</span>
+          <b>{price} руб.</b>
+        </div>
+        <button className="button">
+          {/* <img width={11} height={11} src="/img/plus.svg" alt="Plus" /> */}
+          <svg onClick={(obj) => console.log(obj)} width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.6653 5.13128H7.20219V1.66827C7.20219 0.332907 5.13118 0.332907 5.13118 1.66827V5.13128H1.66805C0.332981 5.13128 0.332981 7.20221 1.66805 7.20221H5.13118V10.6652C5.13118 12.0006 7.20219 12.0006 7.20219 10.6652V7.20221H10.6653C12.0006 7.20221 12.0006 5.13128 10.6653 5.13128Z" fill="#D3D3D3"/>
+          </svg>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
